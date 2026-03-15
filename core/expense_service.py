a538b6fddb00b517b0a19d11b403dac582ca40f1
Veri <verivlk@gmail.com>
2026-03-15 20:39:14 +0000

    pass behaviour test

--- core/expense_service.py
+++ core/expense_service.py
@@ -69,12 +69,10 @@ class ExpenseService:
         return self._repository.list_all()
 
     def total_amount(self) -> float:
-        """
-        # FIXME:
-        Debería de devolver la suma de los amounts de todos los Expenses, ahora mismo parece devolver 0 solamente.
-        :return:
-        """
-        return 0
+        total = 0.0
+        for expense in self._repository.list_all():
+            total += expense.amount
+        return total
 
     def total_by_month(self) -> dict[str, float]:
         totals: DefaultDict[str, float] = defaultdict(float)
