05c26645370de657dbafbc71f760d5ce4b5d6155
Veri <verivlk@gmail.com>
2026-03-15 19:32:01 +0000

    pass no title test

--- core/expense.py
+++ core/expense.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import date
 
 from core.domain_error import (
+    EmptyTitleError,
     InvalidAmountError,
     InvalidExpenseDateError,
 )
@@ -16,9 +17,9 @@ class Expense:
     expense_date: date
 
     def __post_init__(self):
-        """
-        FIXME: Revisen si falta algo que comprobar...
-        """
+
+        if not self.title:
+            raise EmptyTitleError("El título no puede estar vacío")
 
         if self.amount <= 0:
             raise InvalidAmountError("El importe debe ser mayor que 0")
